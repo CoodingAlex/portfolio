716c468ae20eb05c030c45daec52ac3ba2cfddb8
coodingalex <coodingalex@gmail.com>
2020-06-24 01:54:19 -0600

    ui completed

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    Projects
+    <div class="container">
+      <div class="projects__container">
+        <div class="project__description">
+          <h1>Proyecto</h1>
+          <span
+            >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores
+            labore quam quibusdam officia reprehenderit optio non, odio minima,
+            ipsa suscipit doloremque temporibus quis molestias, tenetur sint
+            sapiente ducimus beatae corrupti.</span
+          >
+        </div>
+        <div class="project__img__container">
+          <img src="../assets/logoJs.png" class="project__img" alt="" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,4 +25,22 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  margin: 30px 50px;
+  background: black;
+}
+.projects__container {
+  display: flex;
+  color: whitesmoke;
+  justify-content: space-between;
+}
+.project__description {
+  font-family: 'Roboto', sans-serif;
+  margin: 30px;
+}
+.project__img {
+  width: 300px;
+  margin: 30px;
+}
+</style>

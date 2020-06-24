93458820bc0b45b0e6d33ac603c9f1d880b6b4f4
coodingalex <coodingalex@gmail.com>
2020-06-24 15:38:55 -0600

    before ui fixes

---
+++ src/projectsMocks.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    title: 'Project',
+    content: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores
+          labore quam quibusdam officia reprehenderit optio non, odio minima,
+          ipsa suscipit doloremque temporibus quis molestias, tenetur sint
+          sapiente ducimus beatae corrupti.`
+  },
+  {
+    title: 'Project',
+    content: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores
+          labore quam quibusdam officia reprehenderit optio non, odio minima,
+          ipsa suscipit doloremque temporibus quis molestias, tenetur sint
+          sapiente ducimus beatae corrupti.`
+  },
+  {
+    title: 'Project',
+    content: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores
+          labore quam quibusdam officia reprehenderit optio non, odio minima,
+          ipsa suscipit doloremque temporibus quis molestias, tenetur sint
+          sapiente ducimus beatae corrupti.`
+  }
+]

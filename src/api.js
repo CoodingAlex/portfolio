69bdc716fbeac7655038b011cc133b549a9320e5
coodingalex <coodingalex@gmail.com>
2020-06-30 00:05:32 -0600

    api integration

--- src/api.js
+++ src/api.js
@@ -1,6 +1,5 @@
 const apiUrl =
-  process.env.API_URL || 'https://coodingalex-portfolio-5lh2fwblb.vercel.app'
-console.log(process.env.API_URL)
+  process.env.API_URL || 'https://portfolio-api.coodingalex.vercel.app'
 
 async function getProjects() {
   const data = await fetch(`${apiUrl}/portfolio/projects`)
